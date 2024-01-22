aeefb303b1f97a84ed4acab44c5aa274fd64a045
Juan F Buitrago <jfernand196@gmail.com>
2024-01-21 19:10:37 -0500

    Corregir titulo

--- src/index.js
+++ src/index.js
@@ -7,11 +7,6 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Juan Buitrago</title>
-    </head>
     <App />
   </React.StrictMode>
 );
